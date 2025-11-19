0df0f153c22f410759935c0f8e77af20a8e87250
Lachana Kittakunlasap <benzzznb@gmail.com>
2025-11-20 02:13:10 +0700

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
       swalalerts('Please Input Secret Code')
       return;
     }
-    if (secretcode.toLowerCase() === 'sau') {
+    if (secretcode.toLowerCase() === 'dtisau') {
       //window.location.href = '/showall'
       navigate('/showall')
     }
@@ -42,7 +42,7 @@ function Home() {
         <img src={task} alt="task" className='w-40 mb-4' />
         <h1 className='text-2xl font-bold text-shadow-orange-600'>Task App</h1>
         <p className='text-2xl font-bold text-shadow-orange-600'>Digital Technology and Innovation (DTI)</p>
-        <input type="text" placeholder='Secret Code (sau)' value={secretcode} onChange={(e) => setSecretcode(e.target.value)} className='p-2 border border-b-fuchsia-600 mt-6 rounded w-100' />
+        <input type="text" placeholder='Secret Code' value={secretcode} onChange={(e) => setSecretcode(e.target.value)} className='p-2 border border-b-fuchsia-600 mt-6 rounded w-100' />
         <button className="bg-indigo-600 hover:bg-sky-600 text-white cursor-pointer px-5 py-4 rounded mt-6 transition duration-200 ease-in-out" onClick={handletaskclick}>Sign in</button>
 
       </div>
@@ -52,3 +52,4 @@ function Home() {
 }
 
 export default Home
+
